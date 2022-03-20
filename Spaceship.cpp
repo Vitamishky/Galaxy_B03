2d3ea98a622f7a16832b3410a34eb6d3ea99b5d7
Vitamishky <79705402+Vitamishky@users.noreply.github.com>
2022-03-21 00:52:41 +0300

    Добавление функции получения координат

--- Spaceship.cpp
+++ Spaceship.cpp
@@ -7,8 +7,8 @@ spaceShip::spaceShip(sf::RenderWindow& game_window) {
     spaceShip_x = spaceShip_y = 0.5f;
     spaceShipSpeed_x = 5.f / surface.x;
     spaceShipSpeed_y = 5.f / surface.y;
-    spaceShipWidth = 128;
-    spaceShipHeight = 128;
+    spaceShipWidth = 128.f;
+    spaceShipHeight = 128.f;
 
     if (!textureShip.loadFromFile("spaceShip.png")) {
         exit(EXIT_FAILURE);
@@ -19,7 +19,7 @@ spaceShip::spaceShip(sf::RenderWindow& game_window) {
 
 void spaceShip::drawSprite(sf::RenderWindow& game_window) {
     spaceShipSprite = sf::Sprite(textureShip);
-    spaceShipSprite.setOrigin(spaceShipWidth / 4, spaceShipHeight / 4);
+    spaceShipSprite.setOrigin(spaceShipWidth, spaceShipHeight);
     auto size = game_window.getSize();
     spaceShipSprite.setPosition(spaceShip_x * size.x, spaceShip_y * size.y);
     game_window.draw(spaceShipSprite);
@@ -33,6 +33,12 @@ void spaceShip::moveShip(float dt, char move) {
         case 'u': spaceShip_y -= dt * spaceShipSpeed_y; break;
         case 'd': spaceShip_y += dt * spaceShipSpeed_y; break;
     }
+}
+pair<float, float> spaceShip::getCoordinates() {
+    float x = spaceShip_x;
+    float y = spaceShip_y;
+    pair<float, float> result = make_pair(x, y);
+    return result;
 }
 
 spaceShip::~spaceShip() {}
