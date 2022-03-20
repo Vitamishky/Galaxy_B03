cb0a5c94f5f543a9dd2778493822dba36708661e
Vitamishky <79705402+Vitamishky@users.noreply.github.com>
2022-03-21 00:52:03 +0300

    Набросок камеры слежения

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include "spaceShip.h"
+#include "spaceObjects.h"
+#include "view.h"
 #include <vector>
 
 int main()
 {
+   //Отрисовка окна
+    sf::RenderWindow window(sf::VideoMode(1900, 1050), "Galaxy-B03");
+    //view.reset(sf::FloatRect(0, 0, 1900, 1050));
 
-    sf::RenderWindow window(sf::VideoMode(1900, 1100), "Galaxy-B03", sf::Style::Fullscreen);
     window.setFramerateLimit(30);
-    
+    //Отрисовка иконки около названия окна
     sf::Image icon;
     if (!icon.loadFromFile("spaceShip.png")) {
         return EXIT_FAILURE;
     }
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
+    //Отрисовка заднего фона
     sf::Texture texture;
     if (!texture.loadFromFile("bg.png")) {
         return EXIT_FAILURE;
     }
     sf::Sprite backWall(texture);
-
+    
     spaceShip spaceship(window);
 
     sf::Clock sf_clock;
@@ -41,10 +45,25 @@ int main()
                 window.close();
             }
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) spaceship.moveShip(dt, 'l');
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) spaceship.moveShip(dt, 'r');
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) spaceship.moveShip(dt, 'u');
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) spaceship.moveShip(dt, 'd');
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+                spaceship.moveShip(dt, 'l');
+                //getCoordinatesForView(spaceship.getCoordinates().first, spaceship.getCoordinates().second);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+                spaceship.moveShip(dt, 'r');
+                //getCoordinatesForView(spaceship.getCoordinates().first, spaceship.getCoordinates().second);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+                spaceship.moveShip(dt, 'u');
+                //getCoordinatesForView(spaceship.getCoordinates().first, spaceship.getCoordinates().second);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+                spaceship.moveShip(dt, 'd');
+                //getCoordinatesForView(spaceship.getCoordinates().first, spaceship.getCoordinates().second);
+            }
         }
 
         window.draw(backWall);
@@ -52,6 +71,8 @@ int main()
         spaceship.drawSprite(window);
 
         window.display();
+
+        //window.setView(view);
 
         window.clear();
     }
